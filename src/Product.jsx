3b0cf4d7406709354db5e1aecea834658363dd49
Prathamesh <prathameshranmale3647@gmail.com>
2026-06-12 08:17:35 +0530

    array passed

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,11 +1,22 @@
 import "./Product.css";
 
-function Product({ title, price = 1 }) {
+function Product({ title, price, features = [], features2 = {} }) {
   return (
     <div className="Product">
       <h3>CourseName: {title}</h3>
+
       <p>Price: {price}</p>
+
+      <p>Features:</p>
+      <ul>
+        {features.map((feature, index) => (
+          <li key={index}>{feature}</li>
+        ))}
+      </ul>
+
+      <p>{features2.a}</p>
     </div>
   );
 }
+
 export default Product;
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,11 +1,27 @@
 import Product from "./Product.jsx";
+
 function ProductTab() {
+  let options = ["good", "easy"];
+  let options2 = { a: "Simple", b: "ajdna" };
+
   return (
     <>
-      <Product title="Cloud computing" price={8999} />
-      <Product title="Data Science" price={2000} />
-      <Product title="MERN stack" />
+      <Product
+        title="Cloud computing"
+        price={8999}
+        features={options}
+        features2={options2}
+      />
+
+      <Product title="Data Science" price={2000} features={["AI", "ML"]} />
+
+      <Product
+        title="MERN Stack"
+        price={12322}
+        features={["MongoDB", "Express", "React", "Node"]}
+      />
     </>
   );
 }
+
 export default ProductTab;
