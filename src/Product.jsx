2730069f15d0048d5f7673362742526342b1ad88
Prathamesh <prathameshranmale3647@gmail.com>
2026-06-14 20:18:44 +0530

    Dynamic styling added

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,11 +1,14 @@
 import "./Product.css";
 
 function Product({ title, price, features = [], features2 = {} }) {
+  let isDiscount = price > 10000;
+  const styles = { backgroundColor: isDiscount ? "pink" : "" };
+
   return (
-    <div className="Product">
+    <div className="Product" style={styles}>
       <h3>CourseName: {title}</h3>
       <p>Price: {price}</p>
-      {price >= 10000 && <p>Discount is : 5%</p>} {/* conditionals */}
+      {isDiscount && <p>Discount is : 5%</p>} {/* conditionals */}
     </div>
   );
 }
