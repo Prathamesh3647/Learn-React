4901f34e1627ecc0109534ded52b0042260bc5c2
Prathamesh <prathameshranmale3647@gmail.com>
2026-06-14 20:02:57 +0530

    Conditionals

--- src/Product.jsx
+++ src/Product.jsx
@@ -5,6 +5,7 @@ function Product({ title, price, features = [], features2 = {} }) {
     <div className="Product">
       <h3>CourseName: {title}</h3>
       <p>Price: {price}</p>
+      {price >= 10000 && <p>Discount is : 5%</p>} {/* conditionals */}
     </div>
   );
 }
