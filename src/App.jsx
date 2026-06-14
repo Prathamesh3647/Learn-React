bee1285e81988cfd366828887982e165e6b22c36
Prathamesh <prathameshranmale3647@gmail.com>
2026-06-14 20:33:12 +0530

    passing props and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import ProductTab from "./ProductTab.jsx";
-
+import MsgBox from "./MsgBox.jsx";
 function App() {
   return (
     <>
+      <MsgBox userName="Prathamesh" textColor="red" />
+      <MsgBox userName="Shubham" textColor="green" />
       <ProductTab />
     </>
   );
