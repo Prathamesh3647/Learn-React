215d38d1c8fda1c651be56b0d73f5cdbc0f0dbf0
Prathamesh <prathameshranmale3647@gmail.com>
2026-06-18 19:33:54 +0530

    count activity

--- React-state/src/LikeButton.jsx
+++ React-state/src/LikeButton.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 export default function LikeButton() {
   let [isLiked, setIsliked] = useState(false);
+  let [counter, setCounter] = useState(0);
   let toggleLike = () => {
     setIsliked(!isLiked);
+    setCounter(counter + 1);
   };
   let Btncolor = { color: "red" };
   return (
-    <span onClick={toggleLike}>
-      {isLiked ? (
-        <i style={Btncolor} className="fa-solid fa-heart"></i>
-      ) : (
-        <i className="fa-regular fa-heart"></i>
-      )}
-    </span>
+    <>
+      <p>LikeCount : {counter}</p>
+      <span onClick={toggleLike}>
+        {isLiked ? (
+          <i style={Btncolor} className="fa-solid fa-heart"></i>
+        ) : (
+          <i className="fa-regular fa-heart"></i>
+        )}
+      </span>
+    </>
   );
 }
