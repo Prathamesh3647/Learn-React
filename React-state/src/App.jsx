9c8d5b5e76cbd858ef531deef534f217d2f0725c
Prathamesh <prathameshranmale3647@gmail.com>
2026-06-19 10:43:21 +0530

    ludo

--- React-state/src/App.jsx
+++ React-state/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import Ludo from "./Ludo";
 import Counter from "./Counter";
 import LikeButton from "./LikeButton";
 function App() {
   return (
     <>
-      <h1>React state </h1>
-      <LikeButton />
+      <h1>Ludo board</h1>
+      <Ludo />
     </>
   );
 }
